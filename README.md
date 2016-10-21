e3091c32fb1642eddc9473ff29a39074428cb0a3
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-10-21 18:40:28 -0300

    Project kick off

---
+++ README.md
@@ -0,0 +1,19 @@
+# vue-esc
+
+> Vue.js directive to add a document event listener on escape keyup.
+
+### Development Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# dev mode
+$ npm run dev
+
+# test
+$ npm run test
+
+# build
+$ npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path    = require('path');
+const env     = require('yargs').argv.mode;
+const webpack = require('webpack');
+
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+const libraryName = 'vue-esc';
+
+const plugins = [];
+let outputFile;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  outputFile = `${libraryName}.min.js`;
+} else {
+  outputFile = `${libraryName}.js`;
+}
+
+const config = {
+  entry: `${__dirname}/src/index.js`,
+  devtool: 'source-map',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint',
+        include: projectRoot,
+        exclude: /(node_modules|bower_components)/
+      }
+    ],
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        include: projectRoot,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js']
+  },
+  plugins
+};
+
+module.exports = config;

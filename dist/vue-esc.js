180a51b47d936306cff23bab001808397e3f51d7
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-04-20 17:02:33 -0300

    Moving to webpack 2

--- dist/vue-esc.js
+++ dist/vue-esc.js
@@ -16,21 +16,21 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	function __webpack_require__(moduleId) {
 /******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-/******/
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
 /******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+/******/ 		module.l = true;
 /******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -43,21 +43,103 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 /******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	"use strict";
+"use strict";
 
-/***/ }
-/******/ ])
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
-;
-//# sourceMappingURL=vue-esc.js.map
+var vueEsc = {};
+
+vueEsc.onEvent = function (event) {
+  if (event.keyCode === 27) {
+    vueEsc.cb && vueEsc.cb(event);
+  }
+};
+
+vueEsc.bind = function (el) {
+  vueEsc.onEventBound = vueEsc.onEvent.bind({ el: el });
+  document.addEventListener('keyup', vueEsc.onEventBound);
+};
+
+vueEsc.update = function (el, binding) {
+  if (typeof binding.value !== 'function') {
+    throw new Error('Argument must be a function');
+  }
+
+  vueEsc.cb = binding.value;
+};
+
+vueEsc.unbind = function () {
+  document.removeEventListener('keyup', vueEsc.onEventBound);
+};
+
+exports.default = vueEsc;
+module.exports = exports['default'];
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _vueEsc = __webpack_require__(0);
+
+var _vueEsc2 = _interopRequireDefault(_vueEsc);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var plugin = {
+  install: function install(Vue) {
+    Vue.directive('esc', _vueEsc2.default);
+  }
+};
+
+exports.default = plugin;
+module.exports = exports['default'];
+
+/***/ })
+/******/ ]);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,15 @@
+const isProd = process.env.ENV === 'prod'
 const path = require('path')
-const env = require('yargs').argv.mode
-const webpack = require('webpack')
-
-const projectRoot = path.resolve(__dirname, '/')
-
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
 
 const libraryName = 'vue-esc'
-
-const plugins = []
-let outputFile = `${libraryName}.js`
-
-if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }))
-  outputFile = `${libraryName}.min.js`
-}
+const projectRoot = path.resolve(__dirname, '/')
+const outputFile = isProd ? `${libraryName}.min.js` : `${libraryName}.js`
 
 const config = {
   entry: `${__dirname}/src/index.js`,
-  devtool: 'source-map',
+
+  devtool: isProd ? 'source-map' : false,
+
   output: {
     path: `${__dirname}/dist`,
     filename: outputFile,
@@ -26,29 +17,24 @@ const config = {
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
+
   module: {
-    preLoaders: [
+    rules: [
       {
+        enforce: 'pre',
         test: /(\.jsx|\.js)$/,
-        loader: 'eslint',
+        loader: 'eslint-loader',
         include: projectRoot,
         exclude: /(node_modules|bower_components)/
-      }
-    ],
-    loaders: [
+      },
       {
         test: /(\.jsx|\.js)$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         include: projectRoot,
         exclude: /(node_modules|bower_components)/
       }
     ]
-  },
-  resolve: {
-    root: path.resolve('./src'),
-    extensions: ['', '.js']
-  },
-  plugins
+  }
 }
 
 module.exports = config

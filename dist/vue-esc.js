e4fb585a5a6b6cedc82e2dead7982b6224c18d77
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2019-02-23 19:48:19 -0300

    Run build upgrade

--- dist/vue-esc.js
+++ dist/vue-esc.js
@@ -110,6 +110,7 @@ __webpack_require__.r(__webpack_exports__);
 var plugin = {
   install: function install(Vue) {
     Vue.directive('esc', _vue_esc__WEBPACK_IMPORTED_MODULE_0__["default"]);
+    document.addEventListener('keyup', _vue_esc__WEBPACK_IMPORTED_MODULE_0__["default"].onEvent);
   }
 };
 /* harmony default export */ __webpack_exports__["default"] = (plugin);
@@ -125,19 +126,46 @@ var plugin = {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 var vueEsc = {};
+vueEsc.cb = new Map(); // key: HTMLElement, value: callback
 
 vueEsc.onEvent = function (event) {
   if (event.keyCode === 27) {
-    vueEsc.cb && vueEsc.cb(event);
-  }
-};
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
 
-vueEsc.bind = function (el) {
-  vueEsc.onEventBound = vueEsc.onEvent.bind({
-    el: el
-  });
-  document.addEventListener('keyup', vueEsc.onEventBound);
+    try {
+      for (var _iterator = vueEsc.cb.entries()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var _step$value = _slicedToArray(_step.value, 2),
+            element = _step$value[0],
+            cb = _step$value[1];
+
+        cb && cb.call(element, event);
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return != null) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+  }
 };
 
 vueEsc.update = function (el, binding) {
@@ -145,11 +173,11 @@ vueEsc.update = function (el, binding) {
     throw new Error('Argument must be a function');
   }
 
-  vueEsc.cb = binding.value;
+  vueEsc.cb.set(el, binding.value);
 };
 
-vueEsc.unbind = function () {
-  document.removeEventListener('keyup', vueEsc.onEventBound);
+vueEsc.unbind = function (el) {
+  vueEsc.cb.delete(el);
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (vueEsc);
